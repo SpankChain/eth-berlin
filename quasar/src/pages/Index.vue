96c7bdb0b125be275c93b74fcb6218fe0cd7d7e4
Andrei Riaskov <ryaskov.work@gmail.com>
2018-09-08 23:17:07 +0200

    Add Quasar

---
+++ quasar/src/pages/Index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col-xs-12 col-sm-6 col-md-4">
+      </div>
+      <div class="col-xs-12 col-sm-6 col-md-4">
+        <h2 class="q-display-3 fixed-center vertical-top">PeerDuck</h2>
+        <img class="fixed-center vertical-middle" alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+      </div>
+      <div class="col-xs-12 col-sm-6 col-md-4">
+      </div>
+    </div>
+
+  </div>
+
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
