41558ea721d3a56adfe8933926c021748fe1ecee
Sergey Ukustov <sergey.ukustov@machinomy.com>
2018-09-08 19:29:28 +0200

    Maybe play video

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Hls from 'hls.js'
 
-const TX = require('ethereumjs-tx')
 const contractAbi = require('./LedgerChannel.json').abi
 
 const contractAddress = '0xb80996993505eb2d95efb775333b1a5c2708086f'
 const ingridAddress = '0x8ec75ef3adf6c953775d0738e0e7bd60e647e5ef'
+
+const STREAM_ID = '25e9a7dac61f30909106cdecc24e4b95798a7259b04273333e38414e64edf562'
+const SOURCE = 'http://localhost:8935/stream/' + STREAM_ID + '.m3u8'
 
 class App extends Component {
   constructor( ) {
@@ -24,6 +27,7 @@ class App extends Component {
     this.state = {
       masterAccount: '0x',
       delegateAccount: '0x',
+        videoStyle: {display: 'hidden'}
     }
   }
 
@@ -34,6 +38,22 @@ class App extends Component {
       })
     })
     console.log(tx)
+      this.setState({
+          videoStyle: {display: 'block'}
+      })
+  }
+
+  componentDidMount () {
+      if(Hls.isSupported()) {
+          var hls = new Hls();
+          hls.loadSource(SOURCE);
+          hls.attachMedia(video);
+      }
+  }
+
+  handlePlay () {
+      let video = document.getElementById('video')
+      video.play()
   }
 
   render() {
@@ -50,6 +70,13 @@ class App extends Component {
           Delegate Account: {this.state.delegateAccount}
         </p>
         <button onClick={this.registerDelegateKey}>Register Delegate Key</button>
+        <hr />
+          <div style={this.state.videoStyle}>
+            <button onClick={this.handlePlay.bind(this)}>Play</button>
+            <div className='video-container'>
+                <video id="video"/>
+            </div>
+          </div>
       </div>
     );
   }
