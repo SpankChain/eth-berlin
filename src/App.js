f53092e623ede73c2c9d0e974fee4a617092822d
Sergey Ukustov <sergey.ukustov@machinomy.com>
2018-09-09 13:39:52 +0200

    CSS

--- src/App.js
+++ src/App.js
@@ -221,6 +221,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div>
+            <h1 className="logo">PeerDuck</h1>
+        </div>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
